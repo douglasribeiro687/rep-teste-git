e365bfb55997a6b0bbf44f60be134af1ebe052c8
Douglas Ribeiro <douglas.dasilva@latam.corp.net>
2019-07-17 19:18:10 -0300

    Arquivo teste_git_2.sql adicionado

---
+++ teste_git_2.sql
@@ -0,0 +1,11 @@
+teste git comandos
+
+git status = verificar status dos arquivos que foram add no repositorio;
+
+git log = listas as ultimas atualizações gravadas;
+
+git add = adicionar arquivos ao repositório local;
+
+git commit = gravar as alterações efetuadas nos aruqivos do repositorio local;
+
+git push = levar as alterações local para o repositorio remoto.
