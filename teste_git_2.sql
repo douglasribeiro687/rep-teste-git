624a6e1d2c981e14802e946a31923f6f7fdebd79
douglasribeiro687 <dougrock.douglas@gmail.com>
2019-07-19 16:50:13 -0300

    Alteracao no arquivo teste_git_2.sql

--- teste_git_2.sql
+++ teste_git_2.sql
@@ -12,8 +12,10 @@ git push = levar as alterações local para o repositorio remoto.
 
 -- novos comandos --
 
- git clone git@github.com:douglasribeiro687/rep-teste-git.git = baixar arquivos de um repositorio remoto
+git clone git@github.com:douglasribeiro687/rep-teste-git.git = baixar arquivos de um repositorio remoto
 
- git pull = sincronizar o projeto do repositorio remoto no repositorio local
+git pull = sincronizar o projeto do repositorio remoto no repositorio local
 
 git checkout = voltar uma versão do repositorio ou voltar apenas um arquivo com base nas gravações feitas
+
+-- novos comandos --
