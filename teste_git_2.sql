a6cd4f77f026fb5de3f7622cba445e77743291bb
douglasribeiro687 <dougrock.douglas@gmail.com>
2019-07-18 23:11:37 -0300

    Ateracao no arquivo teste_git_2.sql e teste_git.txt

--- teste_git_2.sql
+++ teste_git_2.sql
@@ -9,3 +9,9 @@ git add = adicionar arquivos ao repositório local;
 git commit = gravar as alterações efetuadas nos aruqivos do repositorio local;
 
 git push = levar as alterações local para o repositorio remoto.
+
+-- novos comandos --
+
+ git clone git@github.com:douglasribeiro687/rep-teste-git.git = baixar arquivos de um repositorio remoto
+
+ git pull = sincronizar o projeto do repositorio remoto no repositorio local
